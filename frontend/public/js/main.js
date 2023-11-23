97d4a2ab6cf7e5da5dd8cba76cd7438d0b5fc12c
Missael Cortes <missaelcorm@gmail.com>
2023-11-23 19:05:39 +0000

    Redirect to login if no auth

--- frontend/public/js/main.js
+++ frontend/public/js/main.js
@@ -1,3 +1,12 @@
+const token = localStorage.getItem('token');
+const path_no_auth = ['/login.html', '/register.html', '/index.html'];
+
+// If the user is not logged in, redirect to the login page
+if (!token && path_no_auth.indexOf(window.location.pathname) === -1) {
+    alert('You must be logged in to view this page.');
+    window.location.replace('./login.html');
+}
+
 $(function() {
     // Load the navbar using jQuery
     $('#navbar-placeholder').load('./assets/templates/navbar.html');
