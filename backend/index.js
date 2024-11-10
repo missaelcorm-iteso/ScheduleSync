123ec8bcf90ea75dcb0800f0944e4db74461ca15
Missael Cortés Mendoza <60858064+missaelcorm@users.noreply.github.com>
2024-11-10 17:44:17 -0600

    Add documentdb support (#51)
    * Add new env var MONGO_ARGS
    
    * Add MONGO_PORT env var
    
    * Add TLS Support
    
    * Fix no existent dir for certs
    
    * Add validation for mongo protocol if +srv, tls is already enabled by default
    
    * Fix bad comparisson

--- backend/index.js
+++ backend/index.js
@@ -14,14 +14,28 @@ const routes = require('./src/routes');
 const {
     MONGO_PROTOCOL,
     MONGO_HOST,
+    MONGO_PORT,
     MONGO_DB,
     MONGO_USER,
     MONGO_PASS,
+    MONGO_ARGS,
+    CERT_PATH,
 } = process.env;
 
 const APP_PORT = process.env.APP_PORT || 3000;
 
-const MONGO_URI = `${MONGO_PROTOCOL}://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}/${MONGO_DB}?retryWrites=true&w=majority`;
+const _MONGO_PORT = MONGO_PROTOCOL == "mongodb+srv" ? '' : `:${MONGO_PORT}`;
+
+const MONGO_URI = `${MONGO_PROTOCOL}://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}${_MONGO_PORT}/${MONGO_DB}?${MONGO_ARGS}`;
+
+let MONGO_CONNECTION_OPTIONS = {};
+
+if (CERT_PATH && MONGO_PROTOCOL !== "mongodb+srv") {
+    MONGO_CONNECTION_OPTIONS = {
+        tls: true,
+        tlsCAFile: path.resolve(CERT_PATH),
+    };
+}
 
 app.use(cors({ origin: true })); // Enable CORS (Cross-Origin Resource Sharing)
 app.use(express.json());
@@ -75,7 +89,7 @@ app.get('', (req, res) => {
     res.send({"message": "Request received"});
 });
 
-mongoose.connect(MONGO_URI).then((client) => {
+mongoose.connect(MONGO_URI, MONGO_CONNECTION_OPTIONS).then((client) => {
     app.listen(APP_PORT, async () => {
         console.log(`Server running on port ${APP_PORT} and connected to MongoDB`);
 
